c870a94f16e45ec95c3558d097faaf228da43482
Yuval Datner <contact@datner.me>
2023-01-05 00:49:53 +0200

    feat(shared): add shared

---
+++ packages/shared/errors/index.ts
@@ -0,0 +1,20 @@
+import * as Predicate from "@fp-ts/data/Predicate"
+
+export const taggedError =
+  <T extends string>(_tag: T) =>
+  (error: unknown): TaggedError<T> => ({
+    _tag,
+    error,
+  })
+
+export type InferError<E extends ReturnType<typeof taggedError>> = ReturnType<E>
+
+export type TaggedError<T extends string = string> = {
+  _tag: T
+  error: unknown
+}
+
+export const isTaggedError =
+  <Tag extends string>(tag: Tag): Predicate.Refinement<TaggedError, TaggedError<Tag>> =>
+  (err): err is TaggedError<Tag> =>
+    err != null && typeof err === "object" && "_tag" in err && "error" in err && err._tag === tag

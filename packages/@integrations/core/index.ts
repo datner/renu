d71e8cab4b41cb130fa2b2656d85d663c98b7367
Yuval Datner <contact@datner.me>
2023-01-07 02:34:26 +0200

    feat(integrations): add management module to core

--- packages/@integrations/core/index.ts
+++ packages/@integrations/core/index.ts
@@ -1,3 +1,4 @@
 export * as Http from "./http"
 export * as CircuitBreaker from "./circuitBreaker"
 export * as Common from "./common"
+export * as Management from "./management"
---
+++ packages/@integrations/core/management.ts
@@ -0,0 +1,66 @@
+import * as Effect from "@effect/io/Effect"
+import * as Context from "@fp-ts/data/Context"
+import * as C from "@fp-ts/schema/Codec"
+import { ManagementIntegration, ManagementProvider, OrderState, Order, Prisma } from "database"
+import { InferError, taggedError } from "shared/errors"
+
+export const fullOrderInclude = {
+  items: {
+    include: { item: true, modifiers: { include: { modifier: true } } },
+  },
+} satisfies Prisma.OrderInclude
+
+export type FullOrderWithItems = Prisma.OrderGetPayload<{
+  include: typeof fullOrderInclude
+}>
+
+export const Integration = C.struct({
+  id: C.number,
+  venueId: C.number,
+  provider: C.enums(ManagementProvider),
+  // vendorData :: Json
+})
+
+export const managementError = taggedError("ManagementError")
+export type ManagementError = InferError<typeof managementError>
+
+export interface ManagementService {
+  reportOrder(
+    order: FullOrderWithItems
+  ): Effect.Effect<ManagementIntegration, ManagementError, void>
+
+  getOrderStatus(order: Order): Effect.Effect<ManagementIntegration, ManagementError, OrderState>
+
+  // getVenueMenu(): Effect.Effect<ManagementIntegration, ManagementError, ManagementMenu>
+}
+export const ManagementService = Context.Tag<ManagementService>()
+export const IntegrationSettingsService = Context.Tag<ManagementIntegration>()
+
+export interface Identified {
+  id?: string
+  name: string
+}
+
+export interface ManagementModifierOption extends Identified {
+  price?: number
+}
+
+export interface ManagementModifier extends Identified {
+  min?: number
+  max?: number
+  options: ManagementModifierOption[]
+}
+
+export interface ManagementItem extends Identified {
+  description: string
+  price: number
+  modifiers: ManagementModifier[]
+}
+
+export interface ManagementCategory extends Identified {
+  items: ManagementItem[]
+}
+
+export interface ManagementMenu extends Identified {
+  categories: ManagementCategory[]
+}

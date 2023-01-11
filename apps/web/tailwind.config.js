d18195e1534c7b5824c62b335ac9d30ca2b70b9f
Yuval Datner <contact@datner.me>
2023-01-12 01:14:54 +0200

    feat(tailwind): add missing brand colors

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -22,6 +22,50 @@ module.exports = {
           800: "#093C2B",
           900: "#041E15",
         },
+        ocre: {
+          50: "#FFFCF3",
+          100: "#FFF5DE",
+          200: "#FEEABE",
+          300: "#FEE09D",
+          400: "#FDD57D",
+          500: "#FDCB5C",
+          600: "#BE9845",
+          700: "#7F662E",
+          800: "#3F3317",
+        },
+        ginger: {
+          50: "#FEF6F2",
+          100: "#FBE0D3",
+          200: "#F7C1A7",
+          300: "#F3A37B",
+          400: "#EF844F",
+          500: "#EB6523",
+          600: "#B04C1A",
+          700: "#763312",
+          800: "#3B1909",
+        },
+        coral: {
+          50: "#FFF5F6",
+          100: "#FEDCE0",
+          200: "#FDB9C1",
+          300: "#FB96A1",
+          400: "#FA7382",
+          500: "#F95063",
+          600: "#BB3C4A",
+          700: "#7D2832",
+          800: "#3E1419",
+        },
+        blush: {
+          50: "#FDF6FA",
+          100: "#FAE5EF",
+          200: "#F5CBDF",
+          300: "#F0B2D0",
+          400: "#EB98C0",
+          500: "#E67EB0",
+          600: "#AD5F84",
+          700: "#733F58",
+          800: "#39202C",
+        },
       },
       fontFamily: {
         title: ["var(--font-secular-one)", ...defaultTheme.fontFamily.sans],
@@ -39,7 +83,7 @@ module.exports = {
 
           accent: "#FDB9C1",
 
-          neutral: "#FAE5EF",
+          neutral: "#dcdbdd",
 
           "base-100": "#FDFBF9",
 
@@ -55,12 +99,58 @@ module.exports = {
     ],
   },
   plugins: [
-    require("daisyui"),
     require("@tailwindcss/forms")({ strategy: "class" }),
     require("@headlessui/tailwindcss"),
+    require("daisyui"),
     require("@tailwindcss/line-clamp"),
-    plugin(function ({ addVariant }) {
+    plugin(function ({ addVariant, matchVariant }) {
       addVariant("error", ["&[aria-invalid=true]", "&:invalid"])
+      addVariant("error", ["&[aria-invalid=true]", "&:invalid"])
+      matchVariant(
+        "5nth",
+        (value) => {
+          return `&:nth-child(5n+${value})`
+        },
+        {
+          values: {
+            1: "1",
+            2: "2",
+            3: "3",
+            4: "4",
+            5: "5",
+          },
+        }
+      )
+      matchVariant(
+        "nth",
+        (value) => {
+          return `&:nth-child(${value})`
+        },
+        {
+          values: {
+            1: "1",
+            2: "2",
+            3: "3",
+            4: "4",
+            5: "5",
+          },
+        }
+      )
+      matchVariant(
+        "group-5nth",
+        (value) => {
+          return `:merge(.group):nth-child(5n+${value}) &`
+        },
+        {
+          values: {
+            1: "1",
+            2: "2",
+            3: "3",
+            4: "4",
+            5: "5",
+          },
+        }
+      )
     }),
   ],
 }

e28db2fed8898b72fc08ea60c68cd6061cb55109
Yuval Datner <contact@datner.me>
2023-03-28 15:27:02 +0300

    chore(lint-staged): fix lint command

---
+++ .lintstagedrc.js
@@ -0,0 +1,8 @@
+const path = require("path")
+
+const buildEslintCommand = (filenames) =>
+  `pnpm next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(),f)).join(" --file ")}`
+
+module.exports = {
+  "./apps/web/*.{js,jsx,ts,tsx}": [buildEslintCommand],
+}

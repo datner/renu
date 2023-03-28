f934cf0c50b480ad71e06e9c3a03d86b65ea028e
Yuval Datner <contact@datner.me>
2023-03-28 15:43:20 +0300

    fix(items): remove excess properties from form data

--- apps/web/src/items/hooks/form.ts
+++ apps/web/src/items/hooks/form.ts
@@ -1,77 +1,96 @@
-import { useRouter } from "next/router"
-import { Routes } from "@blitzjs/next"
-import { invalidateQuery, invoke, setQueryData, useQuery } from "@blitzjs/rpc"
-import createItem from "../mutations/createItem"
-import updateItem from "../mutations/updateItem"
-import getItem from "../queries/getItem"
-import getItems from "../queries/getItems"
-import { ItemSchema } from "../validations"
-import { pipe } from "@effect/data/Function"
-import * as Effect from "@effect/io/Effect"
-import { none, some } from "fp-ts/Option"
-import getCurrentVenueCategories from "src/categories/queries/getCurrentVenueCategories"
-import { useCallback } from "react"
-import * as Renu from "src/core/effect/runtime"
+import { useRouter } from "next/router";
+import { Routes } from "@blitzjs/next";
+import { invalidateQuery, invoke, setQueryData, useQuery } from "@blitzjs/rpc";
+import createItem from "../mutations/createItem";
+import updateItem from "../mutations/updateItem";
+import getItem from "../queries/getItem";
+import getItems from "../queries/getItems";
+import { ItemSchema } from "../validations";
+import { pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import { none, some } from "fp-ts/Option";
+import getCurrentVenueCategories from "src/categories/queries/getCurrentVenueCategories";
+import { useCallback } from "react";
+import * as Renu from "src/core/effect/runtime";
 
 const invalidateQueries = Effect.allPar([
   Effect.promise(() => invalidateQuery(getItems)),
   Effect.promise(() => invalidateQuery(getCurrentVenueCategories)),
-])
+]);
 
-const revalidate = Effect.sync(() => navigator.sendBeacon("/api/revalidate-current"))
+const revalidate = Effect.sync(() =>
+  navigator.sendBeacon("/api/revalidate-current")
+);
 
 const useCreate = (redirect = false) => {
-  const router = useRouter()
+  const router = useRouter();
 
   const onSubmit = useCallback(
-    (data: ItemSchema) =>
-      pipe(
-        Effect.promise(() => invoke(createItem, data)),
+    (data: ItemSchema) => {
+      const dataNoFile = {
+        ...data,
+        image: {
+          src: data.image.src, // annoying
+        },
+      };
+      return pipe(
+        Effect.promise(() => invoke(createItem, dataNoFile)),
         Effect.tap(() => revalidate),
         Effect.tap((item) =>
-          Effect.sync(() => setQueryData(getItem, { identifier: item.identifier }, item))
+          Effect.sync(() =>
+            setQueryData(getItem, { identifier: item.identifier }, item)
+          )
         ),
         Effect.tap(() => invalidateQueries),
         Effect.tap(({ identifier }) =>
           Effect.ifEffect(
             Effect.succeed(redirect),
-            Effect.sync(() => router.push(Routes.AdminItemsItem({ identifier }))),
-            Effect.unit()
+            Effect.sync(() =>
+              router.push(Routes.AdminItemsItem({ identifier }))
+            ),
+            Effect.unit(),
           )
         ),
-        Renu.runPromise$
-      ),
-    [redirect, router]
-  )
+        Renu.runPromise$,
+      );
+    },
+    [redirect, router],
+  );
 
-  return { onSubmit, item: none }
-}
+  return { onSubmit, item: none };
+};
 
 const useUpdate = (identifier: string) => {
-  const router = useRouter()
-  const [item, { setQueryData }] = useQuery(getItem, { identifier })
+  const router = useRouter();
+  const [item, { setQueryData }] = useQuery(getItem, { identifier });
 
   const onSubmit = (data: ItemSchema) =>
     pipe(
       Effect.sync(() => setQueryData(item)),
-      Effect.flatMap(() => Effect.promise(() => invoke(updateItem, { id: item.id, ...data }))),
+      Effect.flatMap(() =>
+        Effect.promise(() => invoke(updateItem, { id: item.id, ...data }))
+      ),
       Effect.tap((item) => Effect.sync(() => setQueryData(item))),
-      Effect.tap(() => Effect.sync(() => navigator.sendBeacon("/api/revalidate-current"))),
+      Effect.tap(() =>
+        Effect.sync(() => navigator.sendBeacon("/api/revalidate-current"))
+      ),
       Effect.tap(() => invalidateQueries),
       Effect.tap((item) =>
         Effect.ifEffect(
           Effect.succeed(item.identifier === identifier),
           Effect.unit(),
-          Effect.sync(() => router.push(Routes.AdminItemsItem({ identifier: item.identifier })))
+          Effect.sync(() =>
+            router.push(Routes.AdminItemsItem({ identifier: item.identifier }))
+          ),
         )
       ),
-      Renu.runPromise$
-    )
+      Renu.runPromise$,
+    );
 
-  return { onSubmit, item: some(item) }
-}
+  return { onSubmit, item: some(item) };
+};
 
 export const item = {
   useCreate,
   useUpdate,
-}
+};
--- apps/web/src/items/mutations/createItem.ts
+++ apps/web/src/items/mutations/createItem.ts
@@ -1,29 +1,31 @@
-import { resolver } from "@blitzjs/rpc"
-import * as Effect from "@effect/io/Effect"
-import * as Optic from "@fp-ts/optic"
-import { getBlurHash } from "src/core/helpers/plaiceholder"
-import { Prisma } from "database"
-import db from "db"
-import { CreateItem, CreateItemSchema, toCreateItem } from "../validations"
-import { pipe } from "@effect/data/Function"
-import { z } from "zod"
-import * as Parser from "@effect/schema/Parser"
-import { PrismaError } from "src/core/helpers/prisma"
-import * as Renu from "src/core/effect/runtime"
-import * as O from "@effect/data/Option"
+import { resolver } from "@blitzjs/rpc";
+import * as Effect from "@effect/io/Effect";
+import * as Schema from "@effect/schema/Schema";
+import * as Optic from "@fp-ts/optic";
+import { getBlurHash } from "src/core/helpers/plaiceholder";
+import { Prisma } from "database";
+import db from "db";
+import { CreateItem, CreateItemSchema, toCreateItem } from "../validations";
+import { pipe } from "@effect/data/Function";
+import { z } from "zod";
+import * as Parser from "@effect/schema/Parser";
+import { PrismaError } from "src/core/helpers/prisma";
+import * as Renu from "src/core/effect/runtime";
+import * as O from "@effect/data/Option";
 
-export type CreateItemOutput = z.infer<typeof CreateItem>
-const createItem = (data: Prisma.ItemCreateInput) =>
+export type CreateItemOutput = z.infer<typeof CreateItem>;
+const createDbItem = (data: Prisma.ItemCreateInput) =>
   Effect.attemptCatchPromise(
     () =>
       db.item.create({
         include: { content: true, modifiers: true },
         data,
       }),
-    (cause) => new PrismaError("failed to create new item", { cause, resource: "Item" })
-  )
+    (cause) =>
+      new PrismaError("failed to create new item", { cause, resource: "Item" }),
+  );
 
-const input_ = Optic.id<Prisma.ItemCreateInput>()
+const input_ = Optic.id<Prisma.ItemCreateInput>();
 
 const categoryItemConnect = input_
   .at("category")
@@ -31,38 +33,58 @@ const categoryItemConnect = input_
   .at("connect")
   .nonNullable()
   .at("id")
-  .nonNullable()
+  .nonNullable();
 
 const categoryItemPosition = input_
   .at("categoryItems")
   .nonNullable()
   .at("create")
   .nonNullable()
-  .filter((s): s is Prisma.CategoryItemCreateWithoutItemInput => !Array.isArray(s))
-  .at("position")
+  .filter((s): s is Prisma.CategoryItemCreateWithoutItemInput =>
+    !Array.isArray(s)
+  )
+  .at("position");
 
-const setPositionInCategory = <T extends ReturnType<typeof toCreateItem>>(input: T) =>
+const setPositionInCategory = <T extends ReturnType<typeof toCreateItem>>(
+  input: T,
+) =>
   pipe(
     Effect.attemptCatchPromise(
       () =>
         db.categoryItem.count({
-          where: { Category: { id: O.getOrThrow(Optic.getOption(categoryItemConnect)(input)) } },
+          where: {
+            Category: {
+              id: O.getOrThrow(Optic.getOption(categoryItemConnect)(input)),
+            },
+          },
         }),
       (cause) =>
-        new PrismaError("could not get category item count", { cause, resource: "CategoryItem" })
+        new PrismaError("could not get category item count", {
+          cause,
+          resource: "CategoryItem",
+        }),
     ),
-    Effect.map((count) => Optic.replace(categoryItemPosition)(count)(input))
-  )
+    Effect.map((count) => Optic.replace(categoryItemPosition)(count)(input)),
+  );
 
-export default resolver.pipe(Parser.decode(CreateItemSchema), resolver.authorize(), (input, ctx) =>
-  pipe(
-    Effect.succeed(input),
-    Effect.map(toCreateItem),
-    Effect.bind("blurHash", ({ image }) => getBlurHash(image)),
-    Effect.bindValue("organizationId", () => ctx.session.organization.id),
-    Effect.bindValue("Venue", () => ({ connect: { id: ctx.session.venue.id } })),
-    Effect.flatMap(setPositionInCategory),
-    Effect.flatMap(createItem),
-    Renu.runPromise$
-  )
-)
+const createItem = resolver.pipe(
+  (i: Schema.From<typeof CreateItemSchema>) =>
+    Parser.decode(CreateItemSchema)(i, { isUnexpectedAllowed: true }),
+  resolver.authorize(),
+  (input, ctx) =>
+    pipe(
+      Effect.succeed(input),
+      Effect.map(toCreateItem),
+      Effect.bind("blurHash", ({ image }) => getBlurHash(image)),
+      Effect.bindValue("organizationId", () => ctx.session.organization.id),
+      Effect.bindValue(
+        "Venue",
+        () => ({ connect: { id: ctx.session.venue.id } }),
+      ),
+      Effect.flatMap(setPositionInCategory),
+      Effect.flatMap(createDbItem),
+      Renu.runPromise$,
+    ),
+);
+
+export default createItem;

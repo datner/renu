f31926e9e1fa9610d7c0e479ae339bd2d4281628
Yuval Datner <contact@datner.me>
2023-03-31 00:43:24 +0300

    chore(lint-staged): remove js file lint

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,8 +1,8 @@
-const path = require("path")
+const path = require("path");
 
 const buildEslintCommand = (filenames) =>
-  `pnpm next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(),f)).join(" --file ")}`
+  `pnpm next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(" --file ")}`;
 
 module.exports = {
-  "./apps/web/*.{js,jsx,ts,tsx}": [buildEslintCommand],
-}
+  "./apps/web/src/**/*.{ts,tsx}": [buildEslintCommand],
+};

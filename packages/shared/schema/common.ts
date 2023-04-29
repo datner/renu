7798919ab35df66c87dd5752c9cd8027de87faeb
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-04-29 23:50:24 +0300

    feat(shared): update schema

--- packages/shared/schema/common.ts
+++ packages/shared/schema/common.ts
@@ -1,4 +1,5 @@
 import { pipe } from "@effect/data/Function";
+import * as Option from "@effect/data/Option";
 import * as S from "@effect/schema/Schema";
 import { Locale, Prisma } from "database";
 
@@ -19,16 +20,29 @@ export const Id = <B extends string>(brand: B) => pipe(S.number, S.int(), S.posi
 
 export const ForeignId = <B extends string>(brand: B) => pipe(S.string, S.brand(brand));
 
-export const Name = pipe(S.string, S.nonEmpty(), S.maxLength(50), S.trim);
-export const Description = pipe(S.string, S.trim, S.maxLength(180), S.optional);
+// TODO: change max length to 50. Stop sharon from giving monster names 🙄
+export const Name = pipe(S.string, S.nonEmpty(), S.maxLength(180), S.trim);
+export const Description = pipe(S.string, S.maxLength(180), S.optionFromNullable);
 
-export const Content = S.struct({
-  locale: S.enums(Locale),
-  name: Name,
-  description: Description,
-});
+// TODO: Booooooo
+export const Content = S.transform(
+  S.struct({
+    locale: S.enums(Locale),
+    name: Name,
+    description: S.optional(Description),
+  }),
+  S.struct({
+    locale: S.enums(Locale),
+    name: Name,
+    description: S.to(Description),
+  }),
+  c => ({ ...c, description: Option.isOption(c.description) ? c.description : Option.none() }),
+  c => c,
+);
 export interface Content extends S.To<typeof Content> {}
 
 export const fromJson = <I extends S.Json, A>(schema: S.Schema<I, A>) => schema;
 
-export const PrismaJson = S.json as S.Schema<Prisma.JsonValue, S.Json>
+export const PrismaJson = S.json as S.Schema<Prisma.JsonValue, S.Json>;
+
+export const fromPrisma: <I, A>(s: S.Schema<I, A>) => S.Schema<Prisma.JsonValue, A> = (s) => s as any;

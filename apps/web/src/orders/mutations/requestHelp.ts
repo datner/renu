637ac0c5e3f102127d4548f22157444d419619c4
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-15 10:54:26 +0300

    feat(orders): add request for help

---
+++ apps/web/src/orders/mutations/requestHelp.ts
@@ -0,0 +1,31 @@
+import { resolver } from "@blitzjs/rpc";
+import * as O from "@effect/data/Option";
+import * as Effect from "@effect/io/Effect";
+import * as Telegram from "integrations/telegram/sendMessage";
+import { Order } from "shared";
+import { Renu } from "src/core/effect";
+import { Format } from "telegraf";
+
+const requestHelp = resolver.pipe(
+  (_, ctx) => ctx.session.orderId,
+  O.fromNullable,
+  Effect.flatMap(Order.getById),
+  Effect.flatMap(o =>
+    Telegram.notify(
+      Format.join(
+        [
+          Format.bold("❗❗❗❗❗❗ ATTENTION ❗❗❗❗❗❗"),
+          "\n\n",
+          `Order ${o.id} from venue ${o.venueId} has reported a failure and requesting help!`,
+          "\n\n",
+          "The following is the order JSON: \n",
+          Format.pre("json")(JSON.stringify(o, null, 2)),
+        ],
+      ),
+    )
+  ),
+  Effect.asUnit,
+  Renu.runPromise$,
+);
+
+export default requestHelp;

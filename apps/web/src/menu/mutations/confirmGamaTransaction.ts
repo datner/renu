d06b7e86142056f18159e9350de5af0968dbe7dd
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-15 10:55:56 +0300

    feat(menu): update order state

--- apps/web/src/menu/mutations/confirmGamaTransaction.ts
+++ apps/web/src/menu/mutations/confirmGamaTransaction.ts
@@ -1,9 +1,11 @@
 import { resolver } from "@blitzjs/rpc";
+import { pipe } from "@effect/data/Function";
 import * as Effect from "@effect/io/Effect";
 import * as Schema from "@effect/schema/Schema";
 import { Gama } from "@integrations/gama";
 import { Presto } from "@integrations/presto";
 import * as Message from "integrations/telegram/sendMessage";
+import { Order } from "shared";
 import { Renu } from "src/core/effect";
 
 const ConfirmGamaTransaction = Schema.struct({
@@ -14,8 +16,15 @@ const confirmGamaTransaction = resolver.pipe(
   (i: Schema.From<typeof ConfirmGamaTransaction>) => Schema.decodeEffect(ConfirmGamaTransaction)(i),
   Effect.zip(Gama),
   Effect.flatMap(([{ jwt }, gama]) => gama.attachTxId(jwt)),
+  Effect.tap((id) => Order.setOrderState(id, "PaidFor")),
   Effect.zip(Presto),
-  Effect.flatMap(([id, presto]) => presto.postOrder(id)),
+  (self, ctx) =>
+    Effect.flatMap(self, ([id, presto]) =>
+      pipe(
+        presto.postOrder(id),
+        Effect.tap(() => Order.setOrderState(id, "Confirmed")),
+        Effect.tap(() => Effect.promise(() => ctx.session.$setPublicData({ orderId: id }))),
+      )),
   Effect.tap(Message.sendJson),
   Renu.runPromise$,
 );

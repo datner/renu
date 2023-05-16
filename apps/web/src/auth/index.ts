1fd36655549b2dd6ca55a6da9403c6d80b91f006
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-16 10:37:45 +0300

    feat(auth): add resolver

--- apps/web/src/auth/index.ts
+++ apps/web/src/auth/index.ts
@@ -1,1 +1,2 @@
+export * as Resolver from "./resolver";
 export * as Session from "./session";
---
+++ apps/web/src/auth/resolver.ts
@@ -0,0 +1,62 @@
+import { Ctx } from "@blitzjs/next";
+import { absurd, pipe } from "@effect/data/Function";
+import * as Effect from "@effect/io/Effect";
+import * as Match from "@effect/match";
+import { TaggedEnum, taggedEnum } from "@effect/match/TaggedEnum";
+import * as Schema from "@effect/schema/Schema";
+import { AuthenticatedMiddlewareCtx, AuthenticationError, AuthorizationError, CSRFTokenMismatchError } from "blitz";
+import { GlobalRole, MembershipRole } from "database";
+
+export const ResolverError = taggedEnum<{
+  AuthorizationError: AuthorizationError;
+  AuthenticationError: AuthenticationError;
+  CSRFTokenMismatchError: CSRFTokenMismatchError;
+  OrgVenueMismatchError: {};
+}>();
+
+export type ResolverError = TaggedEnum.Infer<typeof ResolverError>;
+
+export const liftError = pipe(
+  Match.type<unknown>(),
+  Match.when(Match.instanceOf(AuthenticationError), ResolverError("AuthenticationError")),
+  Match.when(Match.instanceOf(AuthorizationError), ResolverError("AuthorizationError")),
+  Match.when(Match.instanceOf(CSRFTokenMismatchError), ResolverError("CSRFTokenMismatchError")),
+  Match.orElse(_ => {
+    throw absurd(_ as never);
+  }),
+);
+
+export const schema = <I, A, C extends Ctx>(s: Schema.Schema<I, A>) => (input: I, _ctx: C) =>
+  Schema.decodeEffect(s)(input);
+
+type Role = GlobalRole | MembershipRole;
+
+export const authorize =
+  (roles?: Role | Role[]) => <I extends Effect.Effect<any, any, any>, C extends Ctx>(self: I, ctx: C) => {
+    ctx.session.$authorize(roles);
+    return ({
+      __blitz: true,
+      value: self,
+      ctx: ctx as AuthenticatedMiddlewareCtx,
+    } as const);
+  };
+
+export const flatMap =
+  <A, R1, E1, B, C>(f: (a: A, ctx: C) => Effect.Effect<R1, E1, B>) => <R, E>(self: Effect.Effect<R, E, A>, ctx: C) => ({
+    __blitz: true,
+    value: Effect.flatMap(self, _ => f(_, ctx)),
+    ctx,
+  } as const);
+
+export const map = <A, B, C>(f: (a: A, ctx: C) => B) => <R, E>(self: Effect.Effect<R, E, A>, ctx: C) => ({
+  __blitz: true,
+  value: Effect.map(self, _ => f(_, ctx)),
+  ctx,
+} as const);
+
+export const esnureOrgVenueMatch = <I, C extends AuthenticatedMiddlewareCtx>(_: I, ctx: C) =>
+  Effect.if(
+    ctx.session.venue.organizationId === ctx.session.organization.id,
+    Effect.succeed(_),
+    Effect.fail(ResolverError("OrgVenueMismatchError")()),
+  );

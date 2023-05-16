9e3bef7239c6a490e990f1d47d8034e7584af3b6
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-16 10:37:19 +0300

    feat(shared): add Menu module

---
+++ packages/shared/Menu/requests/getByVenue.ts
@@ -0,0 +1,54 @@
+import * as Data from "@effect/data/Data";
+import { pipe } from "@effect/data/Function";
+import * as Option from "@effect/data/Option";
+import * as A from "@effect/data/ReadonlyArray";
+import * as RA from "@effect/data/ReadonlyRecord";
+import * as Effect from "@effect/io/Effect";
+import * as Request from "@effect/io/Request";
+import * as RequestResolver from "@effect/io/RequestResolver";
+import * as Models from "database";
+import { Database } from "../../Database";
+
+export class GetMenusByVenueError extends Data.TaggedClass("GetMenusByVenueError")<{}> { }
+
+export interface GetMenusByVenue extends Request.Request<GetMenusByVenueError, Models.Menu[]> {
+  readonly _tag: "GetMenusByVenue";
+  readonly id: number;
+}
+
+export const GetMenusByVenue = Request.tagged<GetMenusByVenue>("GetMenusByVenue");
+
+const resolveGetMenusByVenue = pipe(
+  RequestResolver.makeBatched((
+    requests: GetMenusByVenue[],
+  ) =>
+    pipe(
+      Effect.flatMap(Database, db =>
+        Effect.tryCatchPromise(
+          () => db.menu.findMany({ where: { id: { in: requests.map(req => req.id) } } }),
+          () => new GetMenusByVenueError(),
+        )),
+      Effect.map(A.groupBy(c => String(c.id))),
+      Effect.flatMap(data =>
+        Effect.forEach(requests, req =>
+          Request.succeed(
+            req,
+            pipe(
+              RA.get(data, String(req.id)),
+              Option.getOrElse(() => []),
+            ),
+          ))
+      ),
+      Effect.catchAll((_) => Effect.forEach(requests, req => Request.completeEffect(req, Effect.fail(_)))),
+    )
+  ),
+  RequestResolver.contextFromEffect,
+);
+
+export const getByVenue = (id: number) =>
+  Effect.request(
+    GetMenusByVenue({ id }),
+    resolveGetMenusByVenue,
+  );
+
+
--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,7 +1,8 @@
+export * as Category from "./Category";
+export * as Database from "./Database";
 export * as Errors from "./errors";
 export * as Item from "./Item";
-export * as Category from './Category'
+export * as Menu from "./Menu";
 export * as ModifierConfig from "./modifier-config";
 export * as Order from "./Order";
 export * as Venue from "./Venue";
-export * as Database from './Database'

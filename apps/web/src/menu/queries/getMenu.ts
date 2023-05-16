294230980ae516f66e247da508898454956359b4
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-16 23:53:40 +0300

    refactor(menu): cool-ify getMenu

--- apps/web/src/menu/queries/getMenu.ts
+++ apps/web/src/menu/queries/getMenu.ts
@@ -1,8 +1,8 @@
 import { resolver } from "@blitzjs/rpc";
-import { pipe } from "@effect/data/Function";
 import * as Effect from "@effect/io/Effect";
 import * as Schema from "@effect/schema/Schema";
 import { Venue } from "shared";
+import { Resolver } from "src/auth";
 import { Renu } from "src/core/effect";
 
 const GetMenu = Schema.struct({
@@ -10,13 +10,11 @@ const GetMenu = Schema.struct({
 });
 
 export default resolver.pipe(
-  (i: Schema.From<typeof GetMenu>) => Schema.decode(GetMenu)(i),
-  (where) =>
-    pipe(
-      Venue.getByIdentifier(where.identifier),
-      Effect.flatMap(Schema.decodeEffect(Venue.Menu.fromVenue)),
-      Effect.flatMap(Schema.encodeEffect(Venue.Menu.Menu)),
-      Effect.withParallelism(1),
-      Renu.runPromise$,
-    ),
+  Resolver.schema(GetMenu),
+  Effect.map(_ => _.identifier),
+  Effect.flatMap(Venue.getByIdentifier),
+  Effect.flatMap(Schema.decodeEffect(Venue.Menu.fromVenue)),
+  Effect.flatMap(Schema.encodeEffect(Venue.Menu.Menu)),
+  Effect.withParallelism(1),
+  Renu.runPromise$,
 );

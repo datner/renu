f77caf2b9bea5eaee02f95cf9ba201044e0174da
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-05-28 15:19:15 +0300

    fix(menu): remove white background from amount buttons

--- apps/web/src/menu/components/AmountButtons.tsx
+++ apps/web/src/menu/components/AmountButtons.tsx
@@ -9,7 +9,7 @@ export function AmountButtons(props: AmountButtonsProps) {
   const { amount, minimum, onDecrement, onIncrement } = props;
 
   return (
-    <div className="btn-group w-full rtl:flex-row-reverse bg-white">
+    <div className="btn-group w-full rtl:flex-row-reverse">
       <button disabled={amount <= minimum} type="button" className="btn w-10" onClick={onDecrement}>
         -
       </button>

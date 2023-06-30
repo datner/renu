2d8923998273ab4760cd66a35a6db293a114a841
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-01 02:22:54 +0300

    chore(nix): update nix

--- flake.nix
+++ flake.nix
@@ -3,38 +3,32 @@
   nixConfig.bash-prompt = "\[nix:renu\]$ ";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
   };
+  
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      formatter = pkgs.alejandra;
 
-  outputs = {nixpkgs, ...}: let
-    # Helper generating outputs for each desired system
-    forAllSystems = nixpkgs.lib.genAttrs [
-      "x86_64-darwin"
-      "x86_64-linux"
-      "aarch64-darwin"
-      "aarch64-linux"
-    ];
-
-    # Import nixpkgs' package set for each system.
-    nixpkgsFor = forAllSystems (system:
-      import nixpkgs {
-        inherit system;
-      });
-
-    formatter = forAllSystems (system: nixpkgsFor.${system}.alejandra);
-    devShells = forAllSystems (
-      system: let
-        pkgs = nixpkgsFor.${system};
-      in {
+      devShells = {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
             nodejs-18_x
             nodePackages.pnpm
           ];
         };
-      }
-    );
-  in {
-    inherit formatter devShells;
-  };
+      };
+    });
 }

49ab8ffc5d7fe9da65de9f3852219ce6f4bfa958
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-01 02:22:01 +0300

    feat(web): try to make logs readable

--- apps/web/src/blitz-server.ts
+++ apps/web/src/blitz-server.ts
@@ -1,8 +1,14 @@
 import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth";
 import { simpleRolesIsAuthorized } from "@blitzjs/auth";
 import { setupBlitzServer } from "@blitzjs/next";
+import { BlitzLogger } from "blitz";
 import db from "db";
 import { authConfig } from "./blitz-client";
+
+export const defaultLogger = BlitzLogger({
+  prettyLogTemplate: "{{logLevelName}}\t[{{name}}]\t",
+  type: process.env.NODE_ENV === "production" ? "json" : "pretty",
+});
 
 export const { gSSP, gSP, api } = setupBlitzServer({
   plugins: [
@@ -12,4 +18,5 @@ export const { gSSP, gSP, api } = setupBlitzServer({
       isAuthorized: simpleRolesIsAuthorized,
     }),
   ],
+  logger: defaultLogger,
 });

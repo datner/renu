9e0d32aa7ef986057531aa4d5520468a7f70c4e1
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-01 00:10:32 +0300

    refactor(web): change type aesthetics
    I really need to redo the whole telegram bit

--- apps/web/integrations/telegram/index.ts
+++ apps/web/integrations/telegram/index.ts
@@ -2,24 +2,28 @@ import * as Context from "@effect/data/Context";
 import { pipe } from "@effect/data/Function";
 import * as Effect from "@effect/io/Effect";
 import * as Layer from "@effect/io/Layer";
-import { Telegram } from "telegraf";
+import { Telegram as TelegramClient } from "telegraf";
 import { TelegramConfig } from "./client";
 
+export interface Telegram {
+  readonly _: unique symbol;
+}
+
 export interface TelegramService {
-  readonly bot: Telegram;
+  readonly bot: TelegramClient;
   readonly config: Readonly<{
     botToken: string;
     chatId: string;
     datnerId: string;
   }>;
 }
-export const TelegramService = Context.Tag<"Telegram", TelegramService>("Telegram");
+export const TelegramService = Context.Tag<Telegram, TelegramService>("Telegram");
 
 export const layer = Layer.effect(
   TelegramService,
   pipe(
     Effect.config(TelegramConfig),
     Effect.bindTo("config"),
-    Effect.let("bot", ({ config }) => new Telegram(config.botToken)),
+    Effect.let("bot", ({ config }) => new TelegramClient(config.botToken)),
   ),
 );

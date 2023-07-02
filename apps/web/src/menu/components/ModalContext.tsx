bb9de7cea269b15800bd62bb75001163f8019632
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-02 15:33:18 +0300

    fix(web): fix iOS not knowing what overflow is
    this has been a bug for over 13 years now. Why????????

--- apps/web/src/menu/components/ModalContext.tsx
+++ apps/web/src/menu/components/ModalContext.tsx
@@ -4,11 +4,33 @@ type ModalContext = (open: boolean) => void;
 
 const modalContext = createContext<ModalContext>(() => null);
 
+const bodyScroll = {
+  position: 0,
+  lock() {
+    this.position = window.scrollY;
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "fixed";
+    document.body.style.top = `-${this.position}px`;
+    document.body.style.width = "100%";
+  },
+  release() {
+    document.body.style.removeProperty("overflow");
+    document.body.style.removeProperty("position");
+    document.body.style.removeProperty("top");
+    document.body.style.removeProperty("width");
+    window.scrollTo(0, this.position);
+  },
+};
 export function ModalProvider(props: { children?: ReactNode }) {
   const amount = useRef(0);
   const state = useCallback<ModalContext>((open) => {
     open ? ++amount.current : --amount.current;
-    document.body.style.overflowY = amount.current > 0 ? "hidden" : "unset";
+    console.log(amount.current);
+    if (amount.current > 0) {
+      bodyScroll.lock();
+    } else {
+      bodyScroll.release();
+    }
   }, []);
 
   return <modalContext.Provider value={state}>{props.children}</modalContext.Provider>;

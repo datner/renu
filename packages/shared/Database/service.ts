6f0598ac952e1c3909c2cd464e9a97eac5d407b3
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-07 16:07:20 +0300

    refactor(shared): use unique symbol

--- packages/shared/Database/service.ts
+++ packages/shared/Database/service.ts
@@ -1,9 +1,12 @@
 import * as Context from "@effect/data/Context";
 import { PrismaClient } from "database";
 
-export interface Database extends PrismaClient {
-  readonly _tag: "Database"
+export interface DB {
+  readonly _: unique symbol;
 }
-const symbol = Symbol("@renu/shared/Database")
-export const Database = Context.Tag<Database>(symbol)
 
+export interface Database extends PrismaClient {
+  readonly _tag: "Database";
+}
+const symbol = Symbol("@renu/shared/Database");
+export const Database = Context.Tag<DB, Database>(symbol);
